9ebd99c62ebc315ce27302d543dfe3a389d5805b
Vivek Singh <vivekkmr45@yahoo.in>
2019-07-03 01:25:55 +0530

    Add functionality to read file content
    Signed-off-by: Vivek Singh <vivekkmr45@yahoo.in>

--- mapreduce/common.go
+++ mapreduce/common.go
@@ -1,7 +1,35 @@
 package mapreduce
 
+import "os"
+
 //KeyValue key value type pairs
 type KeyValue struct {
 	Key   string
 	Value string
+}
+
+//readFile read file content
+func readFile(fileName string) (int, []byte, error) {
+	var buffer []byte
+	var bytesRead int
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		return bytesRead, buffer, err
+	}
+
+	fileinfo, err := file.Stat()
+	if err != nil {
+		return bytesRead, buffer, err
+	}
+
+	filesize := fileinfo.Size()
+	buffer = make([]byte, filesize)
+
+	bytesRead, err = file.Read(buffer)
+	if err != nil {
+		return bytesRead, buffer, err
+	}
+
+	return bytesRead, buffer, err
 }
--- mapreduce/mapper.go
+++ mapreduce/mapper.go
@@ -1,10 +1,20 @@
 package mapreduce
 
-import "container/list"
+import (
+	"container/list"
+	"fmt"
+)
 
 //Map type of the map function
 type Map func(key, value string) *list.List
 
 func mapper(fileName string, mapFunc Map) {
 
+	_, bytes, err := readFile(fileName)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	result := mapFunc(fileName, string(bytes))
+	fmt.Println(result)
 }
