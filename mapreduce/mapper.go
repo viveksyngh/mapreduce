099a406a381245c5a36b6901de32ee4e2ebd733f
Vivek Singh <vivekkmr45@yahoo.in>
2019-07-10 00:35:47 +0530

    Add some comments and refactoring
    Signed-off-by: Vivek Singh <vivekkmr45@yahoo.in>

--- mapreduce/mapper.go
+++ mapreduce/mapper.go
@@ -20,12 +20,16 @@ func Mapper(fileName string, mapFunc Map, reducerCount int) {
 		return
 	}
 
+	//call user defined map function
 	result := mapFunc(fileName, string(bytes))
+
+	//partition the keys and produce intermediate file
 	for item := result.Front(); item != nil; item = item.Next() {
 		kv := item.Value.(KeyValue)
-		hash := getHash(kv.Key)
+		hash := getHash(kv.Key) //TODO allow to use user defined function
 		reducefilename := getReduceFilename(fileName, int(hash)%reducerCount)
 
+		//create the file if does not exist
 		var reducefile *os.File
 		if exists(reducefilename) {
 			reducefile, err = os.OpenFile(reducefilename, os.O_APPEND|os.O_WRONLY, 0600)
@@ -37,6 +41,8 @@ func Mapper(fileName string, mapFunc Map, reducerCount int) {
 			fmt.Println(err)
 			return
 		}
+
+		//write to the intermediate file
 		_, err = reducefile.WriteString(fmt.Sprintf("%s\t%s\n", kv.Key, kv.Value))
 		if err != nil {
 			fmt.Println(err)
--- mapreduce/reducer.go
+++ mapreduce/reducer.go
@@ -12,10 +12,11 @@ import (
 //Reduce reduce function type
 type Reduce func(key string, values *list.List) KeyValue
 
-func Reducer(fileName string, reduceFunc Reduce, reducerCount int) {
+//Reducer reads intermediate data, runs reduce function and write output data
+func Reducer(fileName string, reduceFunc Reduce, mapperCount int) {
 	intermediateItems := make(map[string]*list.List)
 
-	for i := 0; i < reducerCount; i++ {
+	for i := 0; i < mapperCount; i++ {
 
 		_, bytes, err := readFile(getReduceFilename(fileName, i))
 		if err != nil {
@@ -23,6 +24,7 @@ func Reducer(fileName string, reduceFunc Reduce, reducerCount int) {
 			return
 		}
 
+		//Load and prepare data to be passed to reducer
 		scanner := bufio.NewScanner(strings.NewReader(string(bytes)))
 		scanner.Split(bufio.ScanLines)
 		for scanner.Scan() {
@@ -37,12 +39,14 @@ func Reducer(fileName string, reduceFunc Reduce, reducerCount int) {
 		}
 	}
 
+	//Combine and sort phase
 	var keys []string
 	for key := range intermediateItems {
 		keys = append(keys, key)
 	}
 	sort.Strings(keys)
 
+	//Process data using user defined reduce function and write to output file
 	outputfileName := fmt.Sprintf("%s-out", fileName)
 	file, err := os.Create(outputfileName)
 	if err != nil {
@@ -53,6 +57,6 @@ func Reducer(fileName string, reduceFunc Reduce, reducerCount int) {
 
 	for _, k := range keys {
 		kv := reduceFunc(k, intermediateItems[k])
-		file.WriteString(fmt.Sprintf("%s\t%s\n", kv.Key, kv.Value))
+		file.WriteString(fmt.Sprintf("%s\t%s\n", kv.Key, kv.Value)) //TODO use different encoding method
 	}
 }
